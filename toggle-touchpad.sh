45c6da89617f529eb21fbc4733087297b268e9de
Jeff Baker <sinerasis@gmail.com>
2018-09-04 03:44:33 -0400

    add link to reference

--- toggle-touchpad.sh
+++ toggle-touchpad.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# I cherry picked from this: https://askubuntu.com/questions/844151/enable-disable-touchpad
+# Remember to make this file executable! (chmod +x toggle-touchpad.sh)
+
 let device=`xinput list | grep -i touchpad | sed 's/.*id=\([0-9]*\).*/\1/g'`
 let state=`xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$"`
 
